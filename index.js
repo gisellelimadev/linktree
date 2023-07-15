a687b13f8d6d3d17a2e7d8ebea9f66101ebe88b5
Giselle Lima <124682971+gisellelimadev@users.noreply.github.com>
2023-07-15 16:30:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const day = document.getElementById('day');
+const night = document.getElementById('night');
+
+day.addEventListener("click", function() {
+    document.body.style.backgroundImage = 'url("https://br.web.img3.acsta.net/pictures/19/10/30/11/21/2740070.jpg';
+});
+
+night.addEventListener("click", function() {
+    document.body.style.backgroundImage= 'url("https://c4.wallpaperflare.com/wallpaper/916/657/97/anime-your-name-hd-wallpaper-preview.jpg';
+})
